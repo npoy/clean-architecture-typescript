10977f7f818261e60b315b8b27f849a6419a2190
nickop <nicolas.poy@outlook.com>
2025-06-29 21:41:27 -0300

    Removed hexagonal-architecture example

--- hexagonal-architecture/src/config/tokens.ts
+++
@@ -1,5 +0,0 @@
-export const TOKENS = {
-  BookRepository: "BookRepository",
-  ListBooks: "ListBooks",
-  BookController: "BookController",
-};
--- hexagonal-architecture/src/domain/repositories/BookRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Book } from "../entities/Book.js";
-
-export interface BookRepository {
-  findAll(): Promise<Book[]>;
-  findById(id: string): Promise<Book | null>;
-  save(book: Book): Promise<Book>;
-}
